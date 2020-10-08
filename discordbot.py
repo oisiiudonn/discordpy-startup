d30f4a7cc26ac4df6addb7e840fa70582a2ff034
udonn <61874858+oisiiudonn@users.noreply.github.com>
2020-10-09 00:22:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,16 +11,6 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
-# メッセージ受信時に動作する処理
-@bot.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == 'neko':
-        await message.send('にゃーん')
     
 @bot.command()
 async def ping(ctx):
